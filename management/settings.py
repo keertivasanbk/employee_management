aef0268afa252288d783c43d9654f0c2d9339a52
keertvasanbk <keertivasanbk@gmail.com>
2022-11-22 20:12:21 +0530

    change

--- management/settings.py
+++ management/settings.py
@@ -132,3 +132,6 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
--- management/urls.py
+++ management/urls.py
@@ -19,10 +19,42 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.conf.urls.static import static
 from django.conf import settings
 from employe.views import signout
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login',LoginView.as_view(template_name='login.html'),name='login'),
     path('logout',signout,name='logout'),
+    path(
+        'change-password/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/change-password.html',
+            success_url = '/'
+        ),
+        name='change_password'
+    ),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_form.html',
+             subject_template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_subject.txt',
+             email_template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_email.html',
+             # success_url='/login/'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='/home/keerthivasan/Public/internship_review/employee_management/employee/management/template/common/password-reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('',include('employe.urls')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
